588c24dec2bcf9c9a86d1fb2f6c5bcf54017ce42
t-yama11 <am80sltz@icloud.com>
2022-04-06 01:09:10 +0900

    整数判定の処理を修正した.

--- exception.py
+++ exception.py
@@ -7,20 +7,30 @@ except 例外:
 """
 
 class TypeIntError(TypeError):
-    print("整数ではありません.")
-    pass
+    def __init__(self, num):
+        self.num = num
 
-if __name__ == "__main__":
+    def __str__(self):
+        return f'{__class__.__name__} : {self.num}は整数ではありません.'
+
+
+def main():
     a = 2
     b = 3
 
+    val = a/b
+    if not isinstance(val, int):
+        raise TypeIntError(val)
+        
+    print(val)
+
+
+if __name__ == "__main__":
+
     # a/bの結果に対する例外処理
     try:
-        val = a/b
+        main()
     except ZeroDivisionError as e:
-        print(e, f"{a}/{b}")
-    else:
-        if isinstance(val, int):
-            print(val)
-        else:
-            TypeIntError(f"{val}は整数ではありません.")
+        print(e)
+    except TypeIntError as e:
+        print(e)
