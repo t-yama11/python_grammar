8f8ac9e1fffc5389dd72c403e631c7833066a8e8
t-yama11 <am80sltz@icloud.com>
2022-04-14 23:38:41 +0900

    finallyを追加した.

--- exception.py
+++ exception.py
@@ -4,6 +4,10 @@ try:
     例外の起こる可能性のある処理
 except 例外:
     例外が起きた際の処理
+else:
+    例外が起きなかった場合の処理
+finally:
+    例外発生の有無に関係なく行う処理
 """
 class TypeIntError(TypeError):
     def __init__(self, num):
@@ -34,3 +38,5 @@ if __name__ == "__main__":
         print(e)
     except TypeIntError as e:
         print(e)
+    finally:
+        print("計算を行いました.")
